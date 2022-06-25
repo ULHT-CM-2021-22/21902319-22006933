6855b47263da2bf8f0b15bae89a99a68a5273b1c
Tiago Santos <a22006933@alunos.ulht.pt>
2022-06-25 10:24:51 +0100

    Initial commit

---
+++ app/src/main/java/com/example/cm_recurso/ui/home/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.cm_recurso.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
+}
