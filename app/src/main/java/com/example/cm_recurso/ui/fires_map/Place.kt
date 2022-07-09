795476863c94b5919da88a16e342e3ba5f6694ca
Tiago Santos <a22006933@alunos.ulht.pt>
2022-07-09 21:04:22 +0100

    Adicionado ao mapa a funcionalidade de navegar para o detalhe do incendio

--- app/src/main/java/com/example/cm_recurso/ui/fires_map/Place.kt
+++
@@ -1,9 +0,0 @@
-package com.example.cm_recurso.ui.fires_map
-
-import com.google.android.gms.maps.model.LatLng
-
-data class Place(
-    val name: String,
-    val latLng: LatLng,
-    val address: LatLng,
-)
