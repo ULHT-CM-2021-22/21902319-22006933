862a0415e3b802a41a2643bccd82f4ce2dc9b23c
Rafael <a21902319@alunos.ulht.pt>
2022-07-10 01:31:26 +0100

    adicionado algoritmos para filtro de organização de lista, decrescente e crescente

--- app/src/main/java/com/example/cm_recurso/MainActivity.kt
+++ app/src/main/java/com/example/cm_recurso/MainActivity.kt
@@ -124,6 +124,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         when (requestCode) {
             LOCATION_PERMISSION_REQ_CODE -> {
                 if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
